93cca89f43a06fb9566fb74f05142db2283503ac
Ariyaphol <s6704062617024@email.kmutnb.ac.th>
2026-03-29 06:24:34 +0700

    อัปเดตโค้ดสมบูรณ์ 100% พร้อม Custom Unpickler

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ class SafeUnpickler(pickle.Unpickler):
             return pathlib.PosixPath
         return super().find_class(module, name)
 
-
 class CustomPickle:
     __name__ = "pickle"
     Unpickler = SafeUnpickler
-    def load(self, f, **kwargs):
-        return SafeUnpickler(f).load()
+    # ต้องรับทั้ง *args และ **kwargs เพื่อให้รองรับทุกค่าที่ PyTorch ส่งมา
+    def load(self, f, *args, **kwargs):
+        return SafeUnpickler(f, *args, **kwargs).load()
 
 custom_pickle = CustomPickle()
 
